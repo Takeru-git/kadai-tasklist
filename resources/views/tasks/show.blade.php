1051c01741514a4a3af38c48be44f4c17aaa2561
takeru.watanabe <takeru.watanabe>
2020-12-30 02:07:20 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -8,6 +8,10 @@
         <tr>
             <th>id</th>
             <td>{{ $task->id }}</td>
+        </tr
+         <tr>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
         </tr>
         <tr>
             <th>メッセージ</th>
